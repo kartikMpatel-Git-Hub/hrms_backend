23cb99028fc8aa93337a2fde5a4e045fe3cafb3c
kartikMpatel-Git-Hub <kartik.patel@roimaint.com>
2026-02-05 23:19:59 +0530

    first commit

---
+++ hrms/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace hrms.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
